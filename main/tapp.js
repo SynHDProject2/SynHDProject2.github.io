17e5bcdbf18832b760a2f0bc9d1b5ae011dd262e
SynHDProject2 <70724214+SynHDProject2@users.noreply.github.com>
2025-12-11 16:42:41 -0800

    Update tapp.js

--- main/tapp.js
+++ main/tapp.js
@@ -93,6 +93,9 @@ let incantationNames = ["King's Blessing", "Super Speed", "Arcanography", "Ultim
 let canvas = document.getElementById("canvas");
 let ctx = canvas.getContext("2d");
 let width, height, fontSize;
+let scale = 0.25*window.innerWidth / canvas.width;
+canvas.style.transform = `scale(${scale})`;
+canvas.style.transformOrigin = "top left";
 
 let message = function(event) {
   if (event.message.startsWith("TP")) {
